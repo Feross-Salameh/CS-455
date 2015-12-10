9962369ef969c8482564905d39d8162296338a67
GregoryJTaranto <gregoryjtaranto@hotmail.com>
2015-12-09 22:35:07 -0800

    Corrected message handler
    Smarter approach to cover more cases.

--- ProxyServer/ProxyServer/Greg.hpp
+++ ProxyServer/ProxyServer/Greg.hpp
@@ -7,9 +7,10 @@
 #include <winsock2.h>
 #include <iostream>
 #include <WS2tcpip.h>
+#include <sstream>
 
 #pragma comment(lib, "Ws2_32.lib")
 
-int fromBrowser(int clientSocketFd); // Handles message traffic from the browser to the internet.
+int messageHandler(int clientSocketFd, char* target_port); // Handles message traffic from the browser to the internet or vice versa.
 
 #endif // GREG_HPP
